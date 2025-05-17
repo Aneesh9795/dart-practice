9a7644e43f5d38786e5769acb667a4e6a30d2f19
Aneesh Patel <158780982+Aneesh9795@users.noreply.github.com>
2025-05-17 22:12:48 +0530

    variable

---
+++ variable.dart
@@ -0,0 +1,11 @@
+void main() {
+  var name = "Rahul";
+  int age = 28;
+  bool isStudent = false;
+  final country = "India";
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Student: $isStudent");
+  print("Country: $country");
+}
