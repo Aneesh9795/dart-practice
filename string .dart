0726497eb622fb6d7e2aad42d89e3b12f54ba877
Aneesh Patel <158780982+Aneesh9795@users.noreply.github.com>
2025-07-17 23:25:43 +0530

    string

--- string .dart
+++ string .dart
@@ -1,5 +1,27 @@
+import 'dart:ffi';
+
+String reverseVowels(String str) {
+  List<String> chars = str.split('');
+  int i = 0;
+  int j = chars.length - 1;
+  String vowels = 'aeiouAEIOU';
+
+  while (i < j) {
+    if (!vowels.contains(chars[i])) {
+      i++;
+    } else if (!vowels.contains(chars[j])) {
+      j--;
+    } else {
+      String temp = chars[i];
+      chars[i] = chars[j];
+      chars[j] = temp;
+      i++;
+      j--;
+    }
+  }
+  return chars.join('');
+}
+
 void main() {
-  String word = "flutter";
-  String removed = word.split("").join();
-  print(removed);
+  print(reverseVowels("hello"));
 }
