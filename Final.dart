3cdd4efc412162a21bc12b6719d7dde66ec14d32
Aneesh Patel <158780982+Aneesh9795@users.noreply.github.com>
2025-07-28 23:13:10 +0530

    final

--- Final.dart
+++ Final.dart
@@ -1,8 +1,7 @@
 void main() {
-  String input = "A N E E S H K U M A R P A T E L";
-  List<String> allowed = ['A', 'K', 'E', 'K', 'A', 'A', 'L'];
-
-  List<String> letters = input.split(" ");
+  String input = "ANEESH KUMAR PATEL";
+  List<String> allowed = ['A', 'k', 'E', 'L'];
+  List<String> letters = input.split("");
   List<String> output = [];
 
   for (String letter in letters) {
@@ -11,5 +10,5 @@ void main() {
     }
   }
 
-  print(output.join(" "));
+  print(output.join());
 }
